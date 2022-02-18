83f48fafef1e7a2d8b34beccb50c96570219b106
Christoffer Björkskog <christoffer.bjorkskog@genero.fi>
2022-02-18 09:47:29 +0200

    1.0.0

--- wp-gds-cmp.php
+++ wp-gds-cmp.php
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WP_GDS_CMP_VERSION', '1.0.0-alpha.1' );
+define( 'WP_GDS_CMP_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
